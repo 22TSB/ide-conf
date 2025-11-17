0acf6a3243e64b672bf504ba11ec0ea9ff162dbe
bogdan22tsb <bogdan22tsb@gmail.com>
2025-11-17 19:57:05 +0200

    v3: nvim (add comment plugin)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,15 @@
 return {
+    {
+        "numToStr/Comment.nvim",
+        dependencies = {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
+        opts = {
+            pre_hook = function(...)
+                return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(...)
+            end,
+        },
+    },
     -- {
     --     "nvim-lua/plenary.nvim",
     --     name = "plenary"
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,59 +1,65 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-			---@diagnostic disable-next-line: missing-fields
-			configs.setup({
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-						keymaps = {
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-						},
-					},
-				},
-				-- enable syntax highlighting
-				highlight = {
-					enable = true,
-				},
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = { enable = true },
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"python",
-					"javascript",
-					"typescript",
-					"tsx",
-					"query",
-					"php",
-					"yaml",
-					"html",
-					"css",
-					"markdown",
-					"markdown_inline",
-					"bash",
-					"lua",
-					"vim",
-					"vimdoc",
-					"c",
-					"cpp",
-					"dockerfile",
-					"gitignore",
-					"astro",
-				},
-				-- auto install above language parsers
-				auto_install = false,
-			})
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "JoosepAlviste/nvim-ts-context-commentstring",
+        },
+        config = function()
+            -- NEW: disable deprecated ts module
+            vim.g.skip_ts_context_commentstring_module = true
+
+            -- NEW: load the plugin’s setup (required with new version)
+            require("ts_context_commentstring").setup({})
+
+            local configs = require("nvim-treesitter.configs")
+            ---@diagnostic disable-next-line: missing-fields
+            configs.setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                        },
+                    },
+                },
+
+                highlight = { enable = true },
+                indent = { enable = true },
+                autotag = { enable = true },
+
+                -- REMOVE the deprecated context_commentstring block entirely
+                -- context_commentstring = { ... }  <-- gone
+
+                ensure_installed = {
+                    "json",
+                    "python",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "query",
+                    "php",
+                    "yaml",
+                    "html",
+                    "css",
+                    "markdown",
+                    "markdown_inline",
+                    "bash",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "c",
+                    "cpp",
+                    "dockerfile",
+                    "gitignore",
+                    "astro",
+                },
+
+                auto_install = false,
+            })
+        end,
+    },
 }
